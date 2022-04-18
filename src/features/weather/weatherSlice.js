a5d44e000da746760886dffbe9dcf584cbbaa1ef
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-04-18 16:00:33 +0100

    Weather function code

--- src/features/weather/weatherSlice.js
+++ src/features/weather/weatherSlice.js
@@ -1,12 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+
 //create weather thunk(action)
 export const fetchWeatherAction = createAsyncThunk(
     'weather/fetch', 
     async (payload, { rejectWithValue, getState, dispatch }) => {
         try {
             const {data} = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`
+                `https://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY2}`
             );
             return data;
         } catch (error) {
