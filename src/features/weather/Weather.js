40634cffeb7ecf229a8ca89a9c763cf1b7173274
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-04-18 23:49:09 +0100

    Add styling to page components

--- src/features/weather/Weather.js
+++ src/features/weather/Weather.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import weatherSVG from "../../img/weather.svg";
 import { fetchWeatherAction, selectState } from './weatherSlice';
+import { DataWrapper, 
+        TitleWrapper, 
+        ImageWrapper,
+        TextWrapper,
+        HeaderWrapper,
+        InputWrapper,
+        MainWrapper,
+        ContainerWrapper } from "../../styles";
 
 function Weather() {
   const [city, setCity] = useState("chicago");
@@ -28,27 +36,25 @@ function Weather() {
 
   return (
     <div>
-      <section class="relative bg-gray-900  min-h-screen">
-        <img
+      <ContainerWrapper>
+        <ImageWrapper
           class="w-56 lg:block lg:absolute top-0 left-0 pt-10"
           src={weatherSVG}
           alt="/"
         />
 
-        <div class="relative container pt-12 px-4 mb-20 mx-auto text-center">
-          <span class="text-blue-500 font-semibold">
-            Built with react and redux
-          </span>
-          <h2 class="mt-8 mb-8 lg:mb-12 text-white text-4xl lg:text-6xl font-semibold">
+        <HeaderWrapper>
+          
+          <TitleWrapper>
             Weather App
-          </h2>
-          <p class="max-w-3xl mx-auto mb-8 lg:mb-12 text-white text-xl opacity-50">
+          </TitleWrapper>
+          <TextWrapper>
             Find out the current weather situation around the world
-          </p>
+          </TextWrapper>
           {/* Input */}
           
           <form onSubmit={handleSearch}>
-            <input
+            <InputWrapper
               type="text" 
               value={city}
               onChange={e => setCity(e.target.value)}
@@ -56,26 +62,26 @@ function Weather() {
               class="relative z-10 inline-block w-full md:w-auto mb-2  px-3 py-2 mr-4  font-medium leading-normal bg-transparent border-2 rounded-lg text-green-400 "
             />
             {/* Button */}
-            <input
+            <InputWrapper
               type="submit" value="Search"
               className="inline-flex items-center px-3 pr-3 28 text-center py-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             />
           </form>  
           
           
-        </div>
+        </HeaderWrapper>
         {/* Content goes here */}
-        <div>
+        <MainWrapper>
           {loading ? (
-            <h1 className="text-gray-400 text-4xl text-center">
+            <TitleWrapper>
               Loading please wait ...
-            </h1>) 
+            </TitleWrapper>) 
           : error ? (
-            <h1 className="text-red-400 text-2xl text-center">
+            <TitleWrapper>
               {error?.message}
-            </h1>)
+            </TitleWrapper>)
           : (
-            <div class="max-w-6xl px-4 mx-auto ">
+            <DataWrapper>
               <div class="flex flex-wrap -mx-4 justify-center">
                 <div class="w-full md:w-1/3 px-4">
                   <div class="p-8 border border-blue-800 rounded-lg">
@@ -122,40 +128,11 @@ function Weather() {
                   </div>
                 </div>
               </div>
-            </div> 
+            </DataWrapper> 
           )}
-        </div>
-      </section>
-      {/* Footer */}
-      <div class="text-center bg-red-900">
-        <p class="mb-4  text-gray-300">
-          Developed by
-          <span class="p-2 text-yellow-300">
-            <a href="https://www.youtube.com/channel/UCvu6J9q1AM6q4xysGqAvVyw">
-              i-Novotek
-            </a>
-          </span>
-        </p>
-        <a
-          class="inline-flex text-blue-400 hover:text-blue-500 font-bold"
-          href="https://www.youtube.com/channel/UCvu6J9q1AM6q4xysGqAvVyw"
-        >
-          <span className="mb-10">Watch the tutorial</span>
-          <svg
-            class="ml-4 w-4 h-5"
-            width="19"
-            height="20"
-            viewBox="0 0 19 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M18.7383 1.47342L18.7383 10.9304L17.5562 10.9304L17.5562 2.89788L0.834948 19.625L0.00154682 18.7916L16.7228 2.06448L9.28125 2.06448L9.28125 0.882355L18.1472 0.882355C18.4737 0.882355 18.7383 1.14697 18.7383 1.47342Z"
-              fill="#1F40FF"
-            ></path>
-          </svg>
-        </a>
-      </div>
+        </MainWrapper>
+      </ContainerWrapper>
+      
     </div>
   );
 };
---
+++ src/styles.js
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+
+export const DataWrapper = styled.div`
+  width: 400px;
+  height: 400px;
+  background: #f0f8ff;
+  border-radius: 15px;
+  border: 5px solid gray;
+  margin: 0 auto;
+  
+`;
+
+export const HeaderWrapper = styled.div`  
+  text-align: center;
+`;
+
+export const ContainerWrapper = styled.section`  
+//   text-align: center;
+`;
+
+export const MainWrapper = styled.div`  
+   margin-bottom: 100px;
+   margin-top: 50px;
+`;
+
+export const InputWrapper = styled.input`  
+  height: 20px;
+  border-radius: 10px;
+`;
+
+export const TitleWrapper = styled.h1`  
+  font-size: 60px;
+  text-align: center;
+`;
+
+export const TextWrapper = styled.h2`
+  font-size: 20px;
+`;
+
+export const ImageWrapper = styled.img`
+  object-fit: cover;
+  width: 200px;
+  height: 200px;
+  border-radius: 15px 15px 0 0;
+`;
