37cc39e6ad2ff077d0f2e3ce0b33622d5b596edc
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-04-21 20:00:47 +0100

    Add signup page style

--- src/features/login/signup/Signup.js
+++ src/features/login/signup/Signup.js
@@ -7,29 +7,43 @@ function Signup() {
 
     return (
         <div className="main-signup">
-          <h1>Welcome! Please Sign up</h1>
-          <div className="Signup-container">
-            <div className="login-form">
-              {/* <form onSubmit={handleSubmit}>
-                <label for="email1">Email</label>  
-                <input placeholder="Enter your email" type="email" id="email1" value={email}
-                    onChange={(e)=> setEmail(e.target.value)}/>
-                <label for="pwd1">Password</label>
-                <input placeholder="Enter your password" type="password" id="pwd1" value={password}
-                    onChange={(e)=> setPassword(e.target.value)}/>
-                <button type="submit" id="submitButton">Submit</button>
-              </form>     */}
-
-              {/* <div className="signup-link">
-                <h4>Don't have an Account ? <Link className='link' to='/signup'>Sign Up now</Link></h4>
-              </div> */}
+          {/* <h1>Welcome! Please Sign </h1> */}
+          
+            <div className="top">
+                <p>Already have an Account?
+                    <Link id='Links-signin' to='/'>Sign In</Link>   
+                </p> 
             </div>
-          </div>  
-          <Link to={'/weather'}>
-            <button>
-                Weather Page
-            </button>
-          </Link>
+            <div className="content-body">
+                <div className="signup-section">
+                    <h1>Create Account!</h1>
+                    <form >
+                        <div className="input-group">
+                            <h5> First Name</h5>
+                            <input type="text" name="Fname"  id="fname" />
+                        </div>
+                        <div className="input-group">
+                            <h5> Last Name</h5>
+                            <input type="text" name="lname"  id="lname" />
+                        </div>
+                        <div className="input-group">
+                            <h5> Email</h5>
+                            <input type="Email" name="email"  id="email1" />
+                        </div>
+                        <div className="input-group">
+                            <h5> Password</h5>
+                            <input type="password"   name="pwd" id="pwd1" />
+                        </div>
+                        <div className="input-group">
+                            <h5> Confirm Password</h5>
+                            <input type="password"  id="pwd2" />
+                        </div>
+                            <input type="submit" id='sbtn' value="Submit" />
+                    </form>
+                </div>
+            </div>
+            
+          
           
 
         </div>
